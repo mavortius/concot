e8b99840d1cb21bbdf2c3529e20fcca9dbcd4abd
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2016-10-27 09:14:30 -0200

    Added date bind formatter.

--- grails-app/domain/concot/Quotation.groovy
+++ grails-app/domain/concot/Quotation.groovy
@@ -1,8 +1,12 @@
 package concot
 
+import grails.databinding.BindingFormat
+
 class Quotation {
 
     BigDecimal value
+
+    @BindingFormat('dd/MM/yyyy')
     Date date
 
     static belongsTo = [item:Item, currency:Currency, supplier:Supplier]
