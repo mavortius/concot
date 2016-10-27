f5f6d86d4ff2e2a4e3cd5eaa75d53e80fa42eefc
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2016-10-27 09:18:40 -0200

    Added email property and toString method.

--- grails-app/domain/concot/Supplier.groovy
+++ grails-app/domain/concot/Supplier.groovy
@@ -3,8 +3,15 @@ package concot
 class Supplier {
 
     String name
+    String email
 
     static constraints = {
         name nullable: false, blank: false, maxSize: 128, unique: true
+        email nullable: false, blank: false, email: true
+    }
+
+    @Override
+    String toString() {
+        name
     }
 }
